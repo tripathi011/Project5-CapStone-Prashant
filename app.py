5cdda82ad637efedeb941259e6947f75f77684ba
Prashant Tripathi <tripathi011@gmail.com>
2022-07-22 20:51:36 +0530

    Run for v2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return """
-	       <h1 style='color: blue;'>This is my capstone project v1 -- Prashant Tripathi</h1>
+	       <h1 style='color: blue;'>This is my capstone project v2 -- Prashant Tripathi</h1>
            """
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
